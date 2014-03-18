523f1369f7d486a388a0c5204676d03f1d0725df
d-turner <dturner@tcd.ie>
2014-03-18 14:00:40 +0000

    Merged the two interface files
    deleted GridBagConstraints and temp file. default layout should be
    enough for now

--- sampleJava/src/UserInterface.java
+++ sampleJava/src/UserInterface.java
@@ -1,73 +1,114 @@
-package userInterface;
-
-
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class UserInterface implements Runnable {
-
-    // creating window
-    private JFrame f = new JFrame("User Interface");
-
-    @Override
-    public void run() {
+public class UserInterface extends JPanel
+				implements ActionListener{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected JButton record;
+    protected JButton stopRecord;
+    protected JComboBox<String> actionList;
+    
+    public UserInterface() {
+        // initialize all buttons and menus
+        ImageIcon playIcon = createImageIcon("images/play.gif");
+	    ImageIcon stopIcon = createImageIcon("images/stop.gif");
+        // create action menu
+        String[] actions= {"Jumping", "Throwing", "Kicking", "Skipping"};
+        actionList= new JComboBox<String>(actions);
+        actionList.setSelectedIndex(0);
+        actionList.addActionListener(new ActionListener(){
+    	    public void actionPerformed(ActionEvent event) {
+    		    // handle selected action here
+    		    // switch(event.getIndex()){}
+    		    
+    	    }
+        });
         // create record button
-        JButton record = new JButton("    record    ");
+        record = new JButton("Record", playIcon);
+        record.setActionCommand("REC");
         // create stopRecord button
-        JButton StopRecord = new JButton("stop recording");
+        stopRecord = new JButton("Stop", stopIcon);
+        stopRecord.setActionCommand("STOP");
+        stopRecord.setEnabled(false);
+ 
+        record.addActionListener(this);
+        stopRecord.addActionListener(this);
+        
+        add(record);
+        add(stopRecord);
+        add(actionList);
+    }
+
+    //Listens to the record and stop
+    public void actionPerformed(ActionEvent event){
+        if ("REC".equals(event.getActionCommand())) {
+          // start recording
+          record.setEnabled(false);
+          stopRecord.setEnabled(true);
+          actionList.setEnabled(false);
+          // printing out to console when button is clicked
+          System.out.println("recording....");
+        }
+        else if("STOP".equals(event.getActionCommand())) {
+    	  // stop recording
+          record.setEnabled(true);
+          stopRecord.setEnabled(false);
+	      actionList.setEnabled(true);
+	      // printing out to console when button is clicked
+	      System.out.println("recording stopped");
+        }
+    }
+
+    // create icons for record and stop recording
+    // can be used to load in files to compare to
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = UserInterface.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        }
+        else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    private static void createAndShowGUI() {
+        final JFrame f = new JFrame("User Interface");
+        //Create and set up the content pane.
+        UserInterface window = new UserInterface();
+        //content panes must be opaque
+        //window.setOpaque(true);
+        f.setContentPane(window);
         // Sets the behavior for when the window is closed
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // set size
-        f.setSize(140,120);
-        // Add a layout manager so that the button is not placed on top of the label
-        f.setLayout( new GridBagLayout());
-        // f.setLayout(new FlowLayout());
-        final GridBagConstraints GBC = new GridBagConstraints();
-        
-        // ADDING TWO BUTTONS TO WINDOW //
-        GBC.gridx = 0; // x coordinate
-        GBC.gridy = 0; // y coordinate
-        f.add(record, GBC); // adding in button
-        
-        GBC.gridx = 0; // x coordinate
-        GBC.gridy = 1; // y coordinate
-        f.add(StopRecord, GBC); // adding in button
-        
+        f.setSize(240,120);
         // Arrange the components inside the window
         //f.pack();
         // By default, the window is not visible. Make it visible.
         f.setVisible(true);
-        
-        record.addActionListener(new ActionListener(){ // setting action to button
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                // TODO Auto-generated method stub
-                System.out.println("recording...."); // printing out to console when button is clicked
-                
-            }   
-        });
-        
-        StopRecord.addActionListener(new ActionListener(){ // setting action to button
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                // TODO Auto-generated method stub
-                System.out.println("recording stopped"); // printing out to console when button is clicked
-                
-            }   
-        });
     }
-
+    
     public static void main(String[] args) {
-        UserInterface mWindow = new UserInterface();
-        // Schedules the application to be run at the correct time in the event queue.
-        SwingUtilities.invokeLater(mWindow);
-    }
-
+	      // Schedules the application to be run at the correct time in the event queue.
+	      javax.swing.SwingUtilities.invokeLater(new Runnable(){
+	    	    @Override
+	    	    public void run() {
+	    	    	createAndShowGUI();
+	    	    }
+	      });
+	  }
 }
