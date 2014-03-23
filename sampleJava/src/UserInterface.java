a00d0357bf879fbc376c2b7bd1e92fe1f401093b
d-turner <dturner@tcd.ie>
2014-03-23 17:28:03 +0000

    added compare button
    Updated combo box listener, added simple main(), added a file loading
    function and added comments. May need to change path names for loading
    and saving files

--- sampleJava/src/UserInterface.java
+++ sampleJava/src/UserInterface.java
@@ -1,6 +1,13 @@
+import java.io.BufferedReader;
 import java.io.File;
+
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -15,19 +22,20 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class UserInterface extends JPanel
-				implements ActionListener{
-    /**
-	 * 
-	 */
+		implements ActionListener{
+
 	private static final long serialVersionUID = 1L;
 	protected JButton record;
     protected JButton stopRecord;
     protected JButton save;
+    protected JButton compare;
 	protected JComboBox actionList;
-    protected int saves;
+    private int saves;
+    // update with type used in comparison algorithm 
+    private String currentActionData= "";
 	
     public UserInterface() {
-        // initialize all buttons and menus
+        // Initialise all buttons and menus
         ImageIcon playIcon = createImageIcon("images/play.gif");
 	    ImageIcon stopIcon = createImageIcon("images/stop.gif");
 	    ImageIcon saveIcon = createImageIcon("images/save.gif");
@@ -39,25 +47,13 @@ public class UserInterface extends JPanel
     	    public void actionPerformed(ActionEvent event) {
     		    // handle selected action here
     	    	JComboBox cb = (JComboBox) event.getSource();
-    	    	int index = (int) cb.getSelectedIndex();
-    	    	switch(index){
-    	    		case 0:
-    	    			System.out.println(cb.getItemAt(index) + " selected");
-    	    			break;
-    	    		case 1:
-    	    			System.out.println(cb.getItemAt(index) + " selected");
-    	    			break;
-    	    		case 2:
-    	    			System.out.println(cb.getItemAt(index) + " selected");
-    	    			break;
-    	    		case 3:
-    	    			System.out.println(cb.getItemAt(index) + " selected");
-    	    			break;
-    	    			
-    	    	}
-    		    
+    	    	System.out.println(cb.getSelectedItem() + " selected");
+    	    	currentActionData= loadFile("src/loads/"+cb.getSelectedItem()+".txt");
     	    }
         });
+        // load default action
+        currentActionData= loadFile("src/loads/"+actionList.getSelectedItem()+".txt");
+        		
         // create record button
         record = new JButton("Record", playIcon);
         record.setActionCommand("REC");
@@ -69,32 +65,40 @@ public class UserInterface extends JPanel
         save = new JButton("", saveIcon);
         save.setActionCommand("SAV");
         save.setEnabled(false);
+        // create compare button
+        compare = new JButton("Compare");
+        compare.setActionCommand("COMP");
+        compare.setEnabled(false);
  
         record.addActionListener(this);
         stopRecord.addActionListener(this);
         save.addActionListener(this);
+        compare.addActionListener(this);
         
         add(record);
         add(stopRecord);
         add(actionList);
         add(save);
+        add(compare);
     }
     
     
-
-    //Listens to the record and stop
+    //Listens to the record, stop, save and compare
     public void actionPerformed(ActionEvent event){
-        if ("REC".equals(event.getActionCommand())) {
+    	String actionCommand = event.getActionCommand();
+        if ("REC".equals(actionCommand)) {
           // start recording
           record.setEnabled(false);
           stopRecord.setEnabled(true);
           actionList.setEnabled(false);
+          compare.setEnabled(false);
+          save.setEnabled(false);
           // printing out to console when button is clicked
           System.out.println("recording....");
           saves++;
         }
-        else if("STOP".equals(event.getActionCommand())) {
-    	  // stop recording
+        else if("STOP".equals(actionCommand)) {
+          // stop recording
           record.setEnabled(true);
           stopRecord.setEnabled(false);
 	      actionList.setEnabled(true);
@@ -102,25 +106,36 @@ public class UserInterface extends JPanel
 	      // printing out to console when button is clicked
 	      System.out.println("recording stopped");
         }
-        else if("SAV".equals(event.getActionCommand())) {
-        	try {
-        		int index = actionList.getSelectedIndex();
-        		
-        		String content = ""+ actionList.getItemAt(index);
-                File newTextFile = new File("src/saves/out"+saves+".txt");
-                FileWriter fileWriter = new FileWriter(newTextFile);
-                fileWriter.write(content);
-                fileWriter.close();
-                
+        else if("SAV".equals(actionCommand)) {
+        	// save file as "out<save number>.txt"
+        	// currently only saves the selected action
+          try {
+        	String content = ""+ actionList.getSelectedItem() + " data";
+            File newTextFile = new File("src/saves/out"+saves+".txt");
+            FileWriter fileWriter = new FileWriter(newTextFile);
+            fileWriter.write(content);
+            fileWriter.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			  e.printStackTrace();
 			}
+          System.out.println("File saved as a " + actionList.getSelectedItem() + " action");
+          compare.setEnabled(true);
+        }
+        else if("COMP".equals(actionCommand)) {
+        	// compares the last saved file with the current selected action file
+        	String cmp = loadFile("src/saves/out"+saves+".txt");
+        	System.out.println("Loaded saved file: " + cmp);
+        	System.out.println("Selected action file: " + currentActionData);
+        	if(cmp.equals(currentActionData)){
+        		System.out.println("Files match");
+        	}
+        	else{
+        		System.err.println("Files don't match");
+        	}
         }
     }
 
-    // create icons for record and stop recording
-    // can be used to load in files to compare to
+    // create icons for buttons
     protected static ImageIcon createImageIcon(String path) {
         java.net.URL imgURL = UserInterface.class.getResource(path);
         if (imgURL != null) {
@@ -130,5 +145,41 @@ public class UserInterface extends JPanel
             System.err.println("Couldn't find file: " + path);
             return null;
         }
+    }
+    
+    // load a file contents into a multi-line string
+    protected static String loadFile(String path) {
+        String str = "";
+        String fileContents= "";
+    	File file = new File(path);
+        BufferedReader reader = null;
+        if (file.exists() && file.isFile()) {
+        	try {
+				reader = new BufferedReader(new FileReader(file));
+				while((str=reader.readLine()) != null){
+					fileContents+= str+"\n";
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally {
+				try{ 
+					reader.close();
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+        }
+        else {
+            System.err.println("Couldn't find file: " + path);
+        }
+        return fileContents;
+    }
+    
+    // simple main for testing
+    public static void main(String[] args){
+    	JFrame mFrame = new JFrame();
+    	mFrame.add(new UserInterface());
+    	mFrame.setSize(400,200);
+		mFrame.setVisible(true); 
     }
  }
